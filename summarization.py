a487fc64f9b5356d7d0458d0ba8b9f2d12039736
Anand Tyagi <ananddtyagi@yahoo.com>
2020-04-13 21:38:56 -0400

    trying to fix the baseline branch

--- summarization.py
+++ summarization.py
@@ -1,37 +1,23 @@
-
-#removes tf debugging
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 import tensorflow as tf
 import tensorflow_hub as hub
 
 
-def extract_sentences():
-    return
 
-def clean_sentences():
-    return
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
-def setnence_to_embeddings(sentences):
-    embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+embeddings = embed([
+    "The quick brown fox jumps over the lazy dog.",
+    "I am a sentence for which I would like to get its embedding"])
 
-    embeddings = embed(sentences)
-    return embeddings
-
-def similarity_matrix():
-    return
-
-def rank_sentences():
-    return
-
-def summarization():
-    return
-
-def main():
-    return
-
-main()
-
-
-
+print(embeddings)
+#
+# word = 'test'
+#
+# sentence = 'this is a test'
+#
+# #reduce the tf output
+# tf.logging.set_verbosity(tf.logging.ERROR)
+#
+# with tf.Session() as session:
+#     session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+#     message_embeddings = session.run(embed(messages))
