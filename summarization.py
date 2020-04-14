0a77939656a758504ffbc85bfd39164b2e7c3b1a
Anand Tyagi <at3684@nyu.edu>
2020-04-13 21:12:36 -0400

    Got the embeddings to work, now need to do it on the sentences of a test article

--- summarization.py
+++ summarization.py
@@ -1,2 +1,23 @@
 import tensorflow as tf
-print(len(tf.config.experimental.list_physical_devices('GPU')))
+import tensorflow_hub as hub
+
+
+
+embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+
+embeddings = embed([
+    "The quick brown fox jumps over the lazy dog.",
+    "I am a sentence for which I would like to get its embedding"])
+
+print(embeddings)
+#
+# word = 'test'
+#
+# sentence = 'this is a test'
+#
+# #reduce the tf output
+# tf.logging.set_verbosity(tf.logging.ERROR)
+#
+# with tf.Session() as session:
+#     session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+#     message_embeddings = session.run(embed(messages))
