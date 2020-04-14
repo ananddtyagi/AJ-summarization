922c40c3dcfe177432488485f77f42326b5958cc
Anand Tyagi <at3684@nyu.edu>
2020-04-14 17:16:23 -0400

    finished cleaning step

--- summarization.py
+++ summarization.py
@@ -19,13 +19,27 @@ def extract_sentences():
     #             break
     #         i = i+1
     with open('article_ex.txt') as file:
-        sentences = sent_tokenize(file.read())
-    for sentence in sentences:
-        sentence = "".join(word_tokenize(sentence))
+        sentences = sent_tokenize(file.read()) #extract all sentneces from article
+
     return sentences
 
-def clean_sentences():
-    return
+def clean_sentences(sentences):
+    import re
+    from nltk.corpus import stopwords
+    stop_words = set(stopwords.words('english'))
+
+    cleaned_sentences = []
+    for sentence in sentences:
+        tokens = word_tokenize(sentence)
+        tokens = [w.lower() for w in tokens] #lowercase all tokens in each sentence
+        tokens = [w for w in tokens if not w in stop_words] #remove stop words
+
+        sentence = " ".join(tokens)
+        sentence = re.sub(r'[^\w]', ' ', sentence) #remove all punctuation
+        sentence = sentence.replace('   ', ' ') #the punctuation step adds spaces, to remove that without removing all spaces, I (Anand) added this step
+        cleaned_sentences.append(sentence)
+
+    return cleaned_sentences
 
 def setnence_to_embeddings(sentences):
     embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
@@ -43,7 +57,10 @@ def summarization():
     return
 
 def main():
+    sentences = []
     sentences = extract_sentences()
+    cleaned_sentences = clean_sentences(sentences)
+
     return
 
 main()
