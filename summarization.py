4f401d76985371784d41b772997d55d444ca23f2
Anand Tyagi <at3684@nyu.edu>
2020-04-14 20:48:56 -0400

    finished the baseline. It works but gives a pretty bad output, I would say. I think abstractive is definitely way better

--- summarization.py
+++ summarization.py
@@ -9,21 +9,13 @@ import tensorflow_hub as hub
 import jsonlines
 from nltk import sent_tokenize, word_tokenize
 
-def extract_sentences():
-    # with jsonlines.open('dev.jsonl') as reader:
-    #     i = 0
-    #     for obj in reader:
-    #         print("text ", obj['text'], "\n")
-    #         print("summary", obj['summary'], "\n")
-    #         if i == 2:
-    #             break
-    #         i = i+1
+def extract():
     with open('article_ex.txt') as file:
         sentences = sent_tokenize(file.read()) #extract all sentneces from article
 
     return sentences
 
-def clean_sentences(sentences):
+def clean(sentences):
     import re
     from nltk.corpus import stopwords
     stop_words = set(stopwords.words('english'))
@@ -41,26 +33,69 @@ def clean_sentences(sentences):
 
     return cleaned_sentences
 
-def setnence_to_embeddings(sentences):
+def sentence_to_embeddings(sentences):
     embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
     embeddings = embed(sentences)
+
     return embeddings
 
-def similarity_matrix():
-    return
+def similarity_score(embeddings):
+    from sklearn.metrics.pairwise import cosine_similarity
 
-def rank_sentences():
-    return
+    similarity = []
+    for embedding_1 in embeddings:
+        similarity.append(0)
+        for embedding_2 in embeddings:
+            #MAKE THIS INTO A TRIANGLE
+            similarity[-1] += cosine_similarity([embedding_1, embedding_2])[0][1]
 
-def summarization():
+    return similarity
+
+def order(scores, sentences):
+    tup_scores = []
+    for i, score in enumerate(scores):
+        tup_scores.append((score, i))
+        
+    ordered_scores = sorted(tup_scores, reverse = True, key=lambda s: s[0]) #sorted in descending order by the score
+    ordered = []
+    for score in ordered_scores:
+        ordered.append(sentences[score[1]])
+
+    return ordered
+
+def summarization(sentences, summary_length):
+
+    return print(''.join(sentences[:summary_length]))
+
+def debug_logger(process, x):
+    print(process)
+    file = open('output.txt', 'w')
+    file.seek(0)
+    file.write(str(x))
+    file.close()
+    print('debug logged')
     return
 
 def main():
     sentences = []
-    sentences = extract_sentences()
-    cleaned_sentences = clean_sentences(sentences)
 
+    sentences = extract()
+    debug_logger('sentences', sentences)
+    cleaned_sentences = clean(sentences)
+    debug_logger('cleaned_sentences', cleaned_sentences)
+    embeddings = sentence_to_embeddings(cleaned_sentences)
+    debug_logger('embeddings', embeddings)
+    similarity = similarity_score(embeddings)
+    debug_logger('similarity', similarity)
+    ordered_sentences = order(similarity, sentences)
+    debug_logger('ordered_sentences', ordered_sentences)
+
+    summary_length = 1
+
+    summary = summarization(ordered_sentences, summary_length)
+
+    print(summary)
     return
 
 main()
